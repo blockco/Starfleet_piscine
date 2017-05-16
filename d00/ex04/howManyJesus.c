946325d1d6e89adfb6ba5ecd902fa33fa4f57b50
robert passafaro <rpassafa@e1z1r8p17.42.us.org>
2017-05-16 15:26:58 -0700

    fixed jesus bible thing

--- d00/ex04/howManyJesus.c
+++ d00/ex04/howManyJesus.c
@@ -1,8 +1,58 @@
 #include <string.h>
+#include <math.h>
+#include <stdio.h>
+
+#define d 256
 
 int howManyJesus(char *bible, char *jesus)
 {
-	int m = strlen(jesus);
-	int n = strlen(bible);
+	int M = strlen(jesus);
+	int N = strlen(bible);
+	int count = 0;
 
+	int i, j;
+	int p = 0;
+	int t = 0;
+	int h = 0;
+	int q = 101;
+
+	i = 0;
+	while (i < M - 1)
+	{
+		h = (int)pow(d, M - 1) % q;
+		i++;
+	}
+	i = 0;
+	while (i < M)
+	{
+		p = (d * p + jesus[i]) % q;
+		t = (d * t + bible[i]) % q;
+		i++;
+	}
+	i = 0;
+	while (i <= N - M)
+	{
+		if (p == t)
+		{
+			j = 0;
+			while (j < M)
+			{
+				if (bible[i + j] != jesus[j])
+				{
+					break;
+				}
+				j++;
+			}
+			if (j == M)
+				count++;
+		}
+		if (i < (N - M))
+		{
+			t = (d * (t - bible[i] * h) + bible[i + M]) % q;
+			if (t < 0)
+				t = (t + q);
+		}
+		i++;
+	}
+	return count;
 }
