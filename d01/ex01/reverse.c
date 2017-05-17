22a49acc8a6580764a5ed4736260b13319f13b3e
robert passafaro <rpassafa@e1z1r8p17.42.us.org>
2017-05-17 00:07:29 -0700

    fixed 01

--- d01/ex01/reverse.c
+++ d01/ex01/reverse.c
@@ -1,12 +1,18 @@
 #include "header.h"
 #include <stdio.h>
 
-void printReverse(struct s_node *lst)
+void util_rev(struct s_node *lst)
 {
 	if (lst->next)
 	{
-		printReverse(lst->next);
+		util_rev(lst->next);
 		printf("%c", ' ');
 	}
 	printf("%s", lst->word);
+}
+
+void printReverse(struct s_node *lst)
+{
+	util_rev(lst);
+	printf("\n");
 }
