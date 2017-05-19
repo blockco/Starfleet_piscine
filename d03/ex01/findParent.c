346e415dfb10240b88a68c3fc2841e3a54b6b139
robert passafaro <rpassafa@e1z1r8p17.42.us.org>
2017-05-18 21:31:40 -0700

    working

--- d03/ex01/findParent.c
+++ d03/ex01/findParent.c
@@ -4,7 +4,8 @@
 void util(struct s_node *root, int i)
 {
 	int c;
-	// printf("%s\n", root->name);
+	if (i == 0) //if root
+		printf("%s\n", root->name);
 	i++;
 	c = 0;
 	while(root->children[c])
@@ -19,12 +20,6 @@ void util(struct s_node *root, int i)
 		util(root->children[c], i);
 		c++;
 	}
-	// c = 0;
-	// while (root->children[c])
-	// {
-	//
-	// 	c++;
-	// }
 }
 
 struct s_node *findParent(struct s_node *root, char *firstSpecies, char *secondSpecies)
