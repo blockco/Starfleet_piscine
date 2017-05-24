8ed3cbd858a73bc99ad56fc19cab370f9016a144
robert passafaro <rpassafa@e1z1r8p17.42.us.org>
2017-05-23 20:27:03 -0700

    one more maybe but mot likely not

--- d04/ex16/piano.c
+++ d04/ex16/piano.c
@@ -1,23 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "header.h"
+#include <math.h>
 
 int **pianoDecompress(struct s_bit *bit, int l)
 {
-	int i;
-	int **ret;
-
-	i = 0;
-	ret = malloc((sizeof(int*) * l) + 1);
-	ret[l] = 0;
-	// while (i < l)
-	// {
-		int j = 0;
-		while (j < bit->n)
-		{
-			printf("%d ", bit->arr[j]);
-			j++;
-		}
-	// }
-	return NULL;
+	int i = 0;
+	int **ret = (int**)malloc((sizeof(int*) * bit->n) + 1);
+	ret[bit->n] = 0;
+	while (i < bit->n)
+	{
+		ret[i] = (int*)calloc(sizeof(int), l);
+		// printf("%d\n", bit->arr[i]);
+		ret[i][(int)log2(bit->arr[i])] = 1;
+		i++;
+	}
+	return ret;
 }
